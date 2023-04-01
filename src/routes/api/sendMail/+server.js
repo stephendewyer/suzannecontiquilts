8e0c88876ceb2f6b8578fbb94681d4e454fe519d
stephendewyer <stephen.dewyer@stephengdewyer.info>
2023-04-01 08:33:12 -0500

    testing .env file

--- src/routes/api/sendMail/+server.js
+++ src/routes/api/sendMail/+server.js
@@ -2,14 +2,11 @@ import sgMail from "@sendgrid/mail";
 import { SENDGRID_API_KEY } from '$env/static/private';
 
 export const GET = async ({ request }) => {
-  console.log(SENDGRID_API_KEY);
 
   const data = await res.json();
 
   return new Response(JSON.stringify(data), { status: 200})
 }
-
-// sgMail.setApiKey(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);
 
 // export async function get(page) {
 //       const msg = {
