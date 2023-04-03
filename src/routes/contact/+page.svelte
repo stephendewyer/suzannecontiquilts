f9b173f83c2c5901924479663cd5b738b35309fe
stephendewyer <stephen.dewyer@stephengdewyer.info>
2023-04-02 20:15:10 -0500

    adding input error messages

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -10,6 +10,119 @@
 	let subject = "";
 	let message = "";
 
+	// before submit
+
+	let enteredNameFirstIsValid = true;
+	let enteredNameLastIsValid = true;
+	let enteredEmailIsValid = true;
+	let enteredSubjectIsValid = true;
+	let enteredMessageIsValid = true;
+
+	let enteredEmailHasAtSymbol = true;
+
+	let enteredNameFirstIsTouched = true;
+	let enteredNameLastIsTouched = true;
+	let enteredEmailIsTouched = true;
+	let enteredSubjectIsTouched = true;
+	let enteredMessageIsTouched = true;
+
+    const nameFirstChangeHandler = () => {
+		console.log(nameFirst)
+        if (nameFirst !== '') {
+            enteredNameFirstIsValid = true;
+        }
+    };
+
+	const nameLastChangeHandler = () => {
+        if (nameLast !== '') {
+            enteredNameLastIsValid = true;
+        }
+    };
+
+	const emailChangeHandler = () => {
+        if (email !== '') {
+            enteredEmailIsValid = true;
+        }
+    };
+
+	const emailLeaveHandler = () => {
+		if (email !== '' && email.includes('@') ) {
+			enteredEmailIsValid = true;
+			enteredEmailHasAtSymbol = true;
+		}
+
+		if (email !== '' && !email.includes('@')) {
+			enteredEmailIsValid = true;
+		}
+	}
+
+	const subjectChangeHandler = () => {
+        if (subject !== '') {
+            enteredSubjectIsValid = true;
+        }
+    };
+
+	const messageChangeHandler = () => {
+        if (message !== '') {
+            enteredMessageIsValid = true;
+        }
+    };
+
+    const nameFirstBlurHandler = () => {
+        enteredNameFirstIsTouched = true;
+        if (nameFirst === '') {
+			enteredNameFirstIsValid = false;
+        }
+    }
+
+    const nameLastBlurHandler = () => {
+        enteredNameLastIsTouched = true;
+        if (nameLast === '') {
+            enteredNameLastIsValid = false;
+        }
+    }
+
+    const emailBlurHandler = () => {
+        enteredEmailIsTouched = true;
+        if (email === '') {
+            enteredEmailIsValid = false;
+        }
+        if (email !== '' && !email.includes('@') ) {
+            enteredEmailIsValid = true;
+            enteredEmailHasAtSymbol = false;
+        }
+    }
+
+	const subjectBlurHandler = () => {
+        enteredSubjectIsTouched = true;
+        if (subject === '') {
+            enteredSubjectIsValid = false;
+        }
+    }
+
+    const messageBlurHandler = () => {
+        enteredMessageIsTouched = true;
+        if (message === '') {
+            enteredMessageIsValid = false;
+        }
+    }
+
+    // after submit
+
+    // const [requestStatus, setRequestStatus] = useState(); // 'pending', 'success', 'error'
+    // const [requestError, setRequestError] = useState();
+
+    // useEffect(() => {
+    //     if (requestStatus === 'success' || requestStatus === 'error') {
+    //         const timer = setTimeout(() => {
+    //             setRequestStatus(null);
+    //             setRequestError(null);
+    //         }, 6000);
+    
+    //         return () => clearTimeout(timer);
+    //     }
+    // }, [requestStatus]);
+
 	async function handleSubmit() {
 
 		const response = await fetch("/api/sendMail", {
@@ -29,9 +142,6 @@
 		});
 		const item = await response.json()
 		console.log(item);
-		// if (!item.ok) {
-		// 	throw new Error(item.message || 'something went wrong');
-		// }
 		return item;
 	}
 
@@ -74,8 +184,15 @@
 							type="text" 
 							class="form_input"
 							bind:value={nameFirst}
-						/>
+							on:input={nameFirstChangeHandler}
+							on:blur={nameFirstBlurHandler}
+						/>	
 					</div>
+					{#if (!enteredNameFirstIsValid)}
+						<div class="input_error_message">
+							Missing a first name.
+						</div>
+					{/if}
 					<div class="form_field">
 						<label 
 							for="name_last"
@@ -89,8 +206,15 @@
 							type="text" 
 							class="form_input"
 							bind:value={nameLast}
+							on:input={nameLastChangeHandler}
+							on:blur={nameLastBlurHandler}
 						/>
 					</div>
+					{#if (!enteredNameLastIsValid)}
+						<div class="input_error_message">
+							Missing a last name.
+						</div>
+					{/if}
 					<div class="form_field">
 						<label 
 							for="email"
@@ -103,8 +227,21 @@
 							type="email" 
 							class="form_input"
 							bind:value={email}
+							on:change={emailLeaveHandler}
+							on:input={emailChangeHandler}
+							on:blur={emailBlurHandler}
 						/>
 					</div>
+					{#if (!enteredEmailIsValid)}
+						<div class="input_error_message">
+							Missing an email.
+						</div>
+					{/if}
+					{#if (!enteredEmailHasAtSymbol)}
+						<div class="input_error_message">
+							Email must have an @ symbol.
+						</div>
+					{/if}
 					<div class="form_field">
 						<label 
 							for="subject"
@@ -118,8 +255,15 @@
 							type="text" 
 							class="form_input"
 							bind:value={subject}
+							on:input={subjectChangeHandler}
+							on:blur={subjectBlurHandler}
 						/>
 					</div>
+					{#if (!enteredSubjectIsValid)}
+						<div class="input_error_message">
+							Missing a subject.
+						</div>
+					{/if}
 					<div class="form_field">
 						<label 
 							for="message"
@@ -132,8 +276,15 @@
 							type="text" 
 							class="form_para_input"
 							bind:value={message}
+							on:input={messageChangeHandler}
+							on:blur={messageBlurHandler}
 						/>
 					</div>
+					{#if (!enteredMessageIsValid)}
+						<div class="input_error_message">
+							Missing a message.
+						</div>
+					{/if}
 					<div class="form_buttons_container">
 						<CancelButton>cancel</CancelButton>
 						<SubmitButton type="submit">send</SubmitButton>
@@ -180,7 +331,7 @@
 
 	.stitches {
 		max-width: 12rem;
-		margin: 1rem auto;
+		margin: 1rem auto auto auto;
 	}
 
 	.form_container {
@@ -193,7 +344,7 @@
 
 	.form_field {
 		width: 100%;
-		margin: 0 0 1rem 0;
+		margin: 1rem 0 0 0;
 		display: flex;
 		flex-direction: row;
 		gap: 0.5rem;
@@ -210,6 +361,13 @@
 		display: inline-block;
 	}
 
+	.input_error_message {
+		width: 100%;
+		text-align: right;
+		font-size: 1.5rem;
+		color: red;
+	}
+
 	.form_para_input {
 		width: 100%;
 		display: inline-block;
@@ -220,6 +378,7 @@
 		flex-direction: row;
 		justify-content: center;
 		gap: 2rem;
+		margin: 1rem auto auto auto;
 	}
 
 	@media (max-width: 750px) {
