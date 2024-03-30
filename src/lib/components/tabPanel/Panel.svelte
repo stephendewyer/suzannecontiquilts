299a1b3421985d159304bb10fde743a8ddf65197
stephen dewyer <stephen.dewyer@stephengdewyer.info>
2024-03-30 15:58:39 -0400

    updating header and footer

---
+++ src/lib/components/tabPanel/Panel.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let tabPanels = [];
+    export let activeTab = 0;
+
+</script>
+
+{#each tabPanels as tabPanel, i}
+    {#if activeTab === tabPanel.index}
+        <svelte:component
+            this={tabPanel.panel}
+            id={`${tabPanel.label}_tabpanel`}
+            role="tabpanel"
+            tabindex={-i}
+            aria-labelledby={`tabpanel_header_${tabPanel.label}`}
+            panel_date={tabPanel.data}
+        />
+    {/if} 
+{/each}
---
+++ src/lib/components/tabPanel/Tabs.svelte
@@ -0,0 +1,32 @@
+<script>
+    import SewingNeedle from "$lib/images/icons/sewing_needle_icon.svg?raw";
+    export let tabPanels = [];
+    export let activeTab = 0;
+
+</script>
+
+<ul class="tabs_container">
+    {#each tabPanels as tab, i}
+        <li
+            class={activeTab === tab.index ? "active_tab" : "tab"}
+            on:click={() => activeTab = tab.index}
+            on:keydown={() => activeTab = tab.index}
+            id={`tabpanel_header_${tab.label}`}
+            role="tab"
+            aria-selected={activeTab === i ? true : false}
+            tabindex={-i}
+        >
+            {tab.label}
+            <div class="sewing_needle_container">
+                {@html SewingNeedle}
+            </div>
+        </li>
+    {/each}
+</ul>
+
+<style>
+    .sewing_needle_container {
+        width: 3rem;
+        height: 0.5rem;
+    }
+</style>
